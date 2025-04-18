114a87e27b5b8c105fba400ec996f5b6db480d2e
Md Mehedi Hasan <mdmehedihasan1515@gmail.com>
2025-04-19 00:09:22 +0600

    everything added

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,3 @@
+export const API_BASE_URL = 'https://advanced-ecommerce-backend.onrender.com/api/v1/';
+export const PAYPAL_CLIENT_ID = 'test'
+export const SERVER_URL = 'https://advanced-ecommerce-backend.onrender.com'
